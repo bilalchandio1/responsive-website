53d4efb8c2b16149731f7e388bec063cceb3a6b0
bilalchandio1 <ahchandio21@gmail.com>
2025-01-19 13:28:21 -0800

    add

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // Your custom theme settings (colors, spacing, etc.)
+    },
+  },
+  plugins: [require("tailwindcss-animate")], // Add the plugin here
+};
